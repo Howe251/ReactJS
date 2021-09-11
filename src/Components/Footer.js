8afc276f955b6b0906b609a1da77f901df5c4d34
howe251 <misha.sosnin245@gmail.com>
2021-09-11 21:20:23 +0300

    Задание 3

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,9 @@
+import s from "../static/css/Footer.module.css"
+
 const Footer = () => {
-  return (<footer>
-    <div class="wrapper">
+  return (
+    <footer>
+    <div class={s.wrapper}>
         <h3>THANKS FOR VISITING</h3>
         <h3>Ведется разработка</h3>
         <p>© 2021 #ReactMarathon.</p>
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,20 +1,20 @@
-import background from "../static/img/bg2.jpg"
+import s from "../static/css/Layout.module.css"
 
-const Layout = ({title, descr, urlBg, colorBg}) => {
+const Layout = ({title, descr, urlBg, colorBg, children}) => {
   let backgr = {}
   if (urlBg && !colorBg) { backgr = {backgroundImage: `url(${urlBg})` }}
   else if (urlBg && colorBg) {  backgr = {background: "#009a63 " + `url(${urlBg})` + " center no-repeat" }}
   else { backgr = {backgroundColor: colorBg} }
   return (
-    <section style={backgr} class="root">
-    <div class="wrapper">
+    <section style={backgr} class={s.root}>
+    <div class={s.wrapper}>
         <article>
-            <div class="title">
+            <div class={s.title}>
                 <h3>{title}</h3>
-                <span class="separator"></span>
+                <span class={s.separator}></span>
             </div>
-            <div class="desc full">
-                <p>{descr}</p>
+            <div class={`${s.desc} ${s.full}`}>
+                {children}
             </div>
         </article>
     </div>
